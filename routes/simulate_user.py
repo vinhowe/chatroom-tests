fef421b762662be645cd122fffc4dcf0852e3062
Grant Pitt <grantpitt0@gmail.com>
2022-09-13 10:17:59 -0600

    got the messaging working sort of

--- routes/simulate_user.py
+++ routes/simulate_user.py
@@ -4,6 +4,10 @@ import aiohttp
 import socketio
 import uuid
 
+stats = {
+    "n_in_waitroom": 0,
+    "n_in_chatroom": 0, 
+}
 
 async def post_signup(url, session, treatment):
     token = uuid.uuid4().hex
@@ -21,42 +25,124 @@ async def post_initial_view(url, session, token, view):
     assert response.status == 200
 
 
+async def get_user(url, session, token):
+    response = await session.get(
+        url + "/user", headers={"X-AUTH-CODE": token}
+    )
+    return await response.json()
+
+
 async def connect_to_waiting_room(url, token):
     sio = socketio.AsyncClient()
+    loop = asyncio.get_running_loop()
+    future = loop.create_future()
 
-    @sio.event
+    namespace = "/waiting-room"
+
+    @sio.event(namespace=namespace)
     async def connect():
         print("connected to waitroom")
+        stats["n_in_waitroom"] += 1
+        print(stats)
 
-    @sio.event
+    @sio.event(namespace=namespace)
     async def redirect(data):
-        print(data, "from waitroom")
+        future.set_result(data)
 
-    @sio.event
+    @sio.event(namespace=namespace)
     async def disconnect():
         print("disconnected from waitroom")
-
-    print("URL:", url + "/waiting-room", "/ws/socket.io")
+        stats["n_in_waitroom"] -= 1
+        print(stats)
 
     await sio.connect(
         url,
         socketio_path="/ws/socket.io",
         wait_timeout=10,
-        namespaces="/waiting-room",
+        namespaces=namespace,
         auth={"token": token},
     )
+    # await sio.wait()
+    redirect_data = await future
+    await sio.disconnect()
+    return redirect_data
+
+
+async def connect_to_chatroom(url, user):
+    sio = socketio.AsyncClient()
+
+    namespace = "/chatroom"
+
+    @sio.event(namespace=namespace)
+    async def connect():
+        print("connected to chatroom")
+        stats["n_in_chatroom"] += 1
+        print(stats)
+        rand = random.randint(0, 1000)
+        await sio.emit(
+            "message", {"body": f"first message {rand}"}, namespace=namespace
+        )
+
+    @sio.event(namespace=namespace)
+    async def messages(data):
+        print("messages:", data)
+
+    @sio.event(namespace=namespace)
+    async def new_message(data):
+        # check if the message is from my sparing mate
+        if user["id"] != data["user_id"]:
+            # print("new message:", data)
+            await asyncio.sleep(1)
+            rand = random.randint(0, 1000)
+            # print("sending message")
+            await sio.emit(
+                "message", {"body": f"another message! {rand}"}, namespace=namespace
+            )
+
+    @sio.event(namespace=namespace)
+    async def disconnect():
+        print("disconnected from chatroom")
+        stats["n_in_chatroom"] -= 1
+        print(stats)
+
+    print("about to connect to the chatroom with token", user["response_id"])
+    await sio.connect(
+        url,
+        socketio_path="/ws/socket.io",
+        wait_timeout=10,
+        namespaces=namespace,
+        auth={"token": user["response_id"]},
+    )
     await sio.wait()
+    # redirect_data = await future
+    # await sio.disconnect()
+    # return redirect_data
 
 
 async def main(url, session, treatment, view):
-    wait = random.randint(1, 5)
-    await asyncio.sleep(wait)
+    # wait = random.randint(1, 5)
+    # await asyncio.sleep(wait)
 
     token = await post_signup(url, session, treatment)
     print(token)
     await post_initial_view(url, session, token, view)
     print("going into the waiting room with", treatment, view)
-    await connect_to_waiting_room(url, token)
+    redirect = await connect_to_waiting_room(url, token)
+
+    # get user id
+    user = await get_user(url, session, token)
+
+    locations = {
+        "view": lambda: post_initial_view(url, session, token, view),
+        "chatroom": lambda: connect_to_chatroom(url, user),
+    }
+
+    print(f"redirecting to {redirect['to']}")
+
+    if redirect["to"] not in locations:
+        raise Exception(f"Unknown redirect: {redirect['to']}")
+
+    await locations[redirect["to"]]()
 
 
 async def make_requests(url):
@@ -66,7 +152,7 @@ async def make_requests(url):
             "treatment": 5,
             "view": "guns are too danguous to be in the hands of lunatics",
         },
-    ]
+    ] * 10
     async with aiohttp.ClientSession() as session:
         await asyncio.gather(
             *[main(url, session, user["treatment"], user["view"]) for user in users]
@@ -74,6 +160,6 @@ async def make_requests(url):
 
 
 if __name__ == "__main__":
-    # url = "http://localhost:8005"
-    url = "http://192.168.200.98:8000"
+    url = "http://localhost:8005"
+    # url = "http://192.168.200.98:8000"
     asyncio.run(make_requests(url))
